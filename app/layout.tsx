052ba09d1ebda5746802c958203ddaedf2261ba4
VanAnWebdev <van.an.webdev@gmail.com>
2026-06-11 14:29:08 +0700

    feat: add Google Analytics gtag tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Permanent_Marker } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin", "vietnamese"], variable: '--font-sans' });
@@ -23,6 +24,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="vi">
+      <head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-L5FZ82BY89" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-L5FZ82BY89');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.variable} ${marker.variable} font-sans bg-space min-h-screen text-slate-50 antialiased overflow-x-hidden`}>
         {children}
       </body>
